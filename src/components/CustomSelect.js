a5056f1e342c5a5ed9eb79e75cda94909e21f24b
Miguel <whyareyouclosed@gmail.com>
2022-08-28 22:38:58 -0500

    Add functionality to custom select

--- src/components/CustomSelect.js
+++ src/components/CustomSelect.js
@@ -10,18 +10,38 @@ const CustomSelect = () => {
         setOptionsOpen(!optionsOpen);
     };
 
+    const handleKeyDown = (index) => (e) => {
+        switch (e.key) {
+          case " ":
+          case "SpaceBar":
+          case "Enter":
+            e.preventDefault();
+            setCurrentOption(index);
+            setOptionsOpen(false);
+            break;
+          default:
+            break;
+        }
+    };
+
     return ( 
         <div className="custom-select">
             <div className="select-wrapper">
-                <button type="button" onClick={toggleOptions} aria-haspopup="listbox" aria-expanded={optionsOpen}>
+                <button className={optionsOpen ? 'expanded' : ''} type="button" onClick={toggleOptions} aria-haspopup="listbox" aria-expanded={optionsOpen}>
                     {options}
                 </button>
                 <ul className={`options-list ${optionsOpen ? 'open' : ''}`} tabIndex={-1} role="listbox" aria-activedescendant={options[currentOption]}>
                     {
                         options.map((option, index) => {
-                            <li id={option} role="option" aria-selected={currentOption == index} tabIndex={0}>
+
+                            return (<li id={option} role="option" aria-selected={currentOption == index} tabIndex={0}
+                                onKeyDown={handleKeyDown(index)}
+                                onClick={() => {
+                                    setCurrentOption(index);
+                                    setOptionsOpen(false);
+                                }}>
                                 {option}
-                            </li>
+                            </li>)
                         })
                     }
                 </ul>
